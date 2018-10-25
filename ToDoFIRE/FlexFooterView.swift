9ff34c9a21b89f65d3b97d0d1277cec3e50c0bcf
Zinko Vyacheslav <zslavman@gmail.com>
2018-10-25 23:33:36 +0300

    push

--- ToDoFIRE/FlexFooterView.swift
+++ ToDoFIRE/FlexFooterView.swift
@@ -19,16 +19,19 @@ class FlexFooterView: UIView, UITextViewDelegate {
 	
 	@IBOutlet weak var footerTextView: UITextView!
 	@IBOutlet weak var addBttn: UIButton!
-	@IBOutlet weak var containerView:UIView!
+	@IBOutlet weak var containerView:UIView! // self
+	@IBOutlet weak var textHeightConstrain:NSLayoutConstraint!
 	
 	private var parentLink:TasksView!
-	private var storedProperty:CGSize! // начальные размеры текстового поля
+	private var storedProperty:CGSize! 		// начальные размеры текстового поля
+	private var startHeight:CGFloat = 0 	// начальная высота текстовой строки
+	
+	
 	
 	
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		nibSetup()
-		print("Меня вызвали!")
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
@@ -56,20 +59,21 @@ class FlexFooterView: UIView, UITextViewDelegate {
 		translatesAutoresizingMaskIntoConstraints = true
 		
 		
-		storedProperty = footerTextView.frame.size
+		storedProperty = containerView.frame.size
+		startHeight = footerTextView.frame.height
 		print("storedProperys = \(storedProperty)")
-		footerTextView.layer.cornerRadius = 10
-		// паддинги для текстового поля
-		footerTextView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
 		
 		addBttn.layer.cornerRadius = addBttn.layer.bounds.size.width / 2
-		addBttn.layer.contentsScale = 1.5
+//		addBttn.layer.contentsScale = 1.5
 		
 		containerView.backgroundColor = #colorLiteral(red: 0.871609158, green: 0.9240212035, blue: 0.9921568627, alpha: 1)
 		containerView.layer.shadowOffset = CGSize(width: 0, height: -2)
 		containerView.layer.shadowRadius = 4
 		containerView.layer.shadowOpacity = 0
 
+		footerTextView.layer.cornerRadius = 10
+		// паддинги для текстового поля
+//		footerTextView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
 		footerTextView.delegate = self
 	}
 	
@@ -79,7 +83,7 @@ class FlexFooterView: UIView, UITextViewDelegate {
 	/// клик на кн. "+"
 	@IBAction func onAddBttnClick(_ sender: UIButton?) {
 		
-		savedHeight = 0
+		startHeight = 0
 		
 		endEditing(true)
 		// после этого сработает
@@ -97,7 +101,7 @@ class FlexFooterView: UIView, UITextViewDelegate {
 		}
 		
 		footerTextView.text = ""
-		footerTextView.frame.size = storedProperty
+		containerView.frame.size = storedProperty
 		
 		// неработающая регулярка
 		//		let regex = try! NSRegularExpression(pattern: "//.|//[|//]|//#|//$", options: [])
@@ -113,23 +117,33 @@ class FlexFooterView: UIView, UITextViewDelegate {
 
 	
 	
-	private var savedHeight:CGFloat = 0
+	
 	
 	// Отслеживаем переход на след. строку при вводе текста
 	internal func textViewDidChange(_ textView: UITextView) {
 
-		let fixedWidth = textView.frame.size.width
+		let fixedWidth = textView.frame.width
 		let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
-		textView.frame.size = CGSize(width: fixedWidth, height: newSize.height)
-
-//		if newSize.height > savedHeight {
-//			let diff = newSize.height - savedHeight
-//			savedHeight = newSize.height
-//			containerView.frame.size.height += diff
-//		}
-
-		print("Высота строки = \(textView.frame.size.height)")
+//		textView.frame.size = CGSize(width: fixedWidth, height: newSize.height)
 		
+//		let newHeight = newSize.height
+//		var diff:CGFloat = 0
+//
+//		if Int(newHeight) != Int(startHeight) {
+//			diff = newHeight - startHeight
+//			startHeight = newHeight
+//
+//			// расширяем фон
+//			containerView.frame.size.height += diff
+//
+//			// расширяем занимаемое пространство
+//			parentLink.tableView_user.contentInset.bottom += diff
+//
+//			print("Высота строки = \(textView.frame.size.height)   diff = \(diff)")
+//		}
+		textHeightConstrain.constant = textView.contentSize.height
+		
+		print("Width = \(textView.contentSize.width) High = \(textView.contentSize.height)")
 	}
 	
 	
